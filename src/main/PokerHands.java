ca40fae0264ca0544dd61f1a326b277041018bcf
Evan Sheehan <res0038@auburn.edu>
2020-05-21 19:40:45 -0400

    Started tentative outline

---
+++ src/main/PokerHands.java
@@ -0,0 +1,50 @@
+package main;
+
+public class PokerHands {
+
+
+    public boolean checkHighCard(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkPair(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkTwoPairs(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkThreeOfAKind(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkStraight(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkFlush(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkFourOfAKind(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkFullHouse(Hand hand) {
+
+        return false;
+    }
+
+    public boolean checkStraightFlush(Hand hand) {
+
+        return false;
+    }
+}
---
+++ src/test/PokerHandsTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PokerHandsTest {
+
+    @Test
+    public void initialTest() {
+        System.out.println("Hello this is a test!");
+        assertEquals(0, 0);
+    }
+
+}
