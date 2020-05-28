842ae13a366ccfb5e5d7698646cda4061f90ee4a
Evan Sheehan <res0038@auburn.edu>
2020-05-28 13:37:36 -0400

    Modified generate hand function to be safer

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -7,11 +7,13 @@ public class Hand {
 
     public static Card[] generateHandFromString(String inputString) {
         String[] stringArr = inputString.split(" ");
-        if (stringArr.length != HAND_SIZE) {
-            System.out.println("The input string is not the correct length of: " + HAND_SIZE);
-            return null;
-        }
         Card[] cards = new Card[HAND_SIZE];
+        if (stringArr.length != HAND_SIZE) {
+            System.out.println("The input string is not the correct length of: " + HAND_SIZE
+                + "\nReturning an empty array.");
+            return cards;
+        }
+        cards = new Card[HAND_SIZE];
         for (int i = 0; i < HAND_SIZE; i++) {
             Card currCard = new Card(stringArr[i]);
             cards[i] = currCard;
