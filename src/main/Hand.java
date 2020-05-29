f9dffb8b680ac5bd311d280f6d37b0234f16749a
Evan Sheehan <res0038@auburn.edu>
2020-05-29 14:08:04 -0400

    Added sort methods to Hand class

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Hand {
 
     private static final int HAND_SIZE = 5;
@@ -18,8 +21,47 @@ public class Hand {
             Card currCard = new Card(stringArr[i]);
             cards[i] = currCard;
         }
+        //Currently sorting each hand that's generated...may change later.
+        cards = sortHandBySuit(cards);
         return cards;
     }
+
+    //Quick implementation of insertion sort. Efficient for this problem because hand size is small(5 cards).
+    public static Card[] sortHandByValue(Card[] cardsIn) {
+        Card[] cards = cardsIn;
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = i; j > 0 && cards[j-1].getCardValueAsInt() > cards[j].getCardValueAsInt(); j--) {
+                Card currValue = cards[j];
+                cards[j] = cards[j-1];
+                cards[j-1] = currValue;
+            }
+        }
+        return cards;
+    }
+
+    //Sorts by suit first (CDHS) then by rank/value of the card.
+    public static Card[] sortHandBySuit(Card[] cardsIn) {
+        Card[] cards = cardsIn;
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = i; j > 0 && cards[j-1].getCardSuitAsInt() > cards[j].getCardSuitAsInt(); j--) {
+                Card currValue = cards[j];
+                cards[j] = cards[j-1];
+                cards[j-1] = currValue;
+            }
+        }
+
+        //Loops through the previously sorted loop, sorting the values within each card's respective suit.
+        //This loop can almost certainly be integrated into the above loop somehow, but the solution is evading me at the moment.
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = i; j > 0 && cards[j-1].getCardSuitAsInt() == cards[j].getCardSuitAsInt() && cards[j-1].getCardValueAsInt() > cards[j].getCardValueAsInt(); j--) {
+                Card currValue = cards[j];
+                cards[j] = cards[j-1];
+                cards[j-1] = currValue;
+            }
+        }
+        return cards;
+    }
+
     /*public Card getCard(int cardIndex) {
         return cards[cardIndex];
     }
