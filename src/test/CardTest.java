862c949d8b552e22b83a5a06ff11bed38eb261de
Evan Sheehan <res0038@auburn.edu>
2020-06-03 16:23:25 -0400

    Improved testing for Card and Hand classes

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -4,49 +4,80 @@ import main.Card;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CardTest {
 
     @Test
     public void incorrectCardLengthExtraSpaceAfter() {
         String cardString = "2H ";
-        Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){fail();}
     }
 
     @Test
     public void incorrectCardLengthExtraSpaceBefore() {
         String cardString = " 2H";
-        Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){fail();}
     }
 
     @Test
     public void incorrectCardLengthExtraDigit() {
         String cardString = "52H";
-        Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){return;}
+        fail();
     }
 
     @Test
     public void incorrectValueZero() {
         String cardString = "0H";
-        Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){return;}
+        fail();
     }
 
     @Test
     public void incorrectValueNonDigit() {
         String cardString = "VH";
-        Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){return;}
+        fail();
     }
 
     @Test
     public void incorrectValueSpecialCharacter() {
         String cardString = "%H";
+        try {
+            Card card = new Card(cardString);
+        }
+        catch (IllegalArgumentException e){return;}
+        fail();
+    }
+
+    @Test
+    public void correctValueLowercaseSuit() {
+        String cardString = "4h";
         Card card = new Card(cardString);
-        Assert.assertEquals(-1, card.getCardValueAsInt());
+        Assert.assertEquals(4, card.getCardValueAsInt());
+    }
+
+    @Test
+    public void correctLowercaseValue() {
+        String cardString = "tH";
+        Card card = new Card(cardString);
+        Assert.assertEquals(10, card.getCardValueAsInt());
     }
 
     @Test
--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -5,6 +5,7 @@ import main.Hand;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class HandTest {
 
@@ -18,14 +19,56 @@ public class HandTest {
     }
 
     @Test
+    public void emptyHandString() {
+        String inputString = "";
+        try {
+            Card[] cards = Hand.generateHandFromString(inputString);
+        }
+        catch (IllegalArgumentException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+    public void lowercaseSuits() {
+        String inputString = "2h 3d 5s 9c Kd ";
+        Card[] cards = Hand.generateHandFromString(inputString);
+        if (cards.length != 5) {fail();}
+    }
+
+    @Test
+    public void lowercaseValues() {
+        String inputString = "tH jH qS tC kD ";
+        Card[] cards = Hand.generateHandFromString(inputString);
+        if (cards.length != 5) {fail();}
+    }
+
+    @Test
+    public void lowercaseValuesAndSuits() {
+        String inputString = "th jh qs tc kd ";
+        Card[] cards = Hand.generateHandFromString(inputString);
+        if (cards.length != 5) {fail();}
+    }
+
+    @Test
+    public void mixedCaseValuesAndSuits() {
+        String inputString = "tH Jh qs TC kD ";
+        Card[] cards = Hand.generateHandFromString(inputString);
+        if (cards.length != 5) {fail();}
+    }
+
+
+    @Test
     public void incorrectHandSize() {
         String inputString = "2H 3D 5S 9C KD AS";
         try {
             Card[] cards = Hand.generateHandFromString(inputString);
         }
-        catch (NullPointerException e) {
-            System.out.println("Caught null pointer, as expected");
+        catch (IllegalArgumentException e) {
+            return;
         }
+        fail();
     }
 
     @Test
