c142817ed1c2abf0cce21af7e60f990a3b75591a
Evan Sheehan <res0038@auburn.edu>
2020-06-03 16:20:41 -0400

    Increased safety of hand generation. Sort functions now use array copies. Fixed bug in hand generator

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -7,28 +7,43 @@ public class Hand {
 
     private static final int HAND_SIZE = 5;
 
-
+    /**
+     *
+     * @param inputString Reads a String to be translated into card objects.
+     * @return Returns an array of Card objects.
+     */
     public static Card[] generateHandFromString(String inputString) {
+        HashSet<String> seenCards = new HashSet<>();
+        inputString = inputString.trim();
         String[] stringArr = inputString.split(" ");
         Card[] cards = new Card[HAND_SIZE];
         if (stringArr.length != HAND_SIZE) {
-            System.out.println("The input string is not the correct length of: " + HAND_SIZE
-                + "\nReturning an empty array.");
-            return cards;
+            throw new IllegalArgumentException("The input string is not the correct length of: " + HAND_SIZE);
         }
         cards = new Card[HAND_SIZE];
         for (int i = 0; i < HAND_SIZE; i++) {
+            if (seenCards.contains(stringArr[i])) {
+                throw new IllegalArgumentException("A hand may not contain two of the same cards!");
+            }
             Card currCard = new Card(stringArr[i]);
             cards[i] = currCard;
+            seenCards.add(stringArr[i]);
         }
         //Currently sorting each hand that's generated...may change later.
-        cards = sortHandBySuit(cards);
+        if (cards.length != 0) {
+            cards = sortHandBySuit(cards);
+        }
         return cards;
     }
 
-    //Quick implementation of insertion sort. Efficient for this problem because hand size is small(5 cards).
+
+    /**
+     * Uses insertion sort, which is quick for small arrays.
+     * @param cardsIn
+     * @return Returns a sorted by value copy of the input array.
+     */
     public static Card[] sortHandByValue(Card[] cardsIn) {
-        Card[] cards = cardsIn;
+        Card[] cards = cardsIn.clone();
         for (int i = 0; i < cards.length; i++) {
             for (int j = i; j > 0 && cards[j-1].getCardValueAsInt() > cards[j].getCardValueAsInt(); j--) {
                 Card currValue = cards[j];
@@ -39,9 +54,15 @@ public class Hand {
         return cards;
     }
 
-    //Sorts by suit first (CDHS) then by rank/value of the card.
+    /**
+     * Uses insertion sort, which is quick for small arrays.
+     * First sorts the array by suit in alphabetical order (C -> D -> H -> S).
+     * Afterwards, sorts by value within each group of suits.
+     * @param cardsIn
+     * @return Returns a sorted by suit copy of the input array.
+     */
     public static Card[] sortHandBySuit(Card[] cardsIn) {
-        Card[] cards = cardsIn;
+        Card[] cards = cardsIn.clone();
         for (int i = 0; i < cards.length; i++) {
             for (int j = i; j > 0 && cards[j-1].getCardSuitAsInt() > cards[j].getCardSuitAsInt(); j--) {
                 Card currValue = cards[j];
@@ -62,15 +83,21 @@ public class Hand {
         return cards;
     }
 
+    /**
+     *
+     * @return Returns a string of 5 randomly drawn unique cards.
+     */
     public static String generateRandomHand() {
         HashSet<String> drawnCards  = new HashSet<>();
         Random random = new Random();
         String inputString = "";
+        String[] possibleValues = {"2", "3", "4", "5", "6", "7", "8", "9",
+            "T", "J", "Q", "K", "A"};
         String[] possibleSuits = {"C", "D", "H", "S"};
         while (drawnCards.size() < 5) {
-            int value = random.nextInt(9) + 1;
-            int suitIndex = random.nextInt(4);
-            String stringToAdd = String.valueOf(value + possibleSuits[suitIndex]);
+            int valueIndex = random.nextInt(possibleValues.length);
+            int suitIndex = random.nextInt(possibleSuits.length);
+            String stringToAdd = possibleValues[valueIndex] + possibleSuits[suitIndex];
             if (!drawnCards.contains(stringToAdd)){
                 inputString += stringToAdd + " ";
                 drawnCards.add(stringToAdd);
